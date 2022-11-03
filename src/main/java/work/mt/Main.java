f04f150aeefe215768ec2e8d7bf97a80c7bb43c6
Mritunjay <email.mt.work@gmail.com>
2022-11-04 01:49:07 +0530

    MT-StringUtils : added stringutils dependency to pom.xml

--- src/main/java/work/mt/Main.java
+++ src/main/java/work/mt/Main.java
@@ -1,8 +1,10 @@
 package work.mt;
 
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(StringUtils.capitalize("hello world!"));
     }
 }
